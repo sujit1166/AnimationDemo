af6cbca3f3a2de892f77917db9528f5ef9339a52
Sujit Tayade <sujit.t@servify.in>
2020-02-04 23:58:22 +0530

    add temp animation in xml, attach animations to views

--- app/src/main/java/com/sujit/animationdemo/MainActivity.java
+++ app/src/main/java/com/sujit/animationdemo/MainActivity.java
@@ -2,17 +2,21 @@ package com.sujit.animationdemo;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Animation.AnimationListener {
 
     RelativeLayout rlRoot, rlCircularWithUsers, rlLanguageSelection;
     Button btnNext;
 
     int counter = 0;
+    Animation moveProfle1Animation, moveProfle2Animation, moveProfle3Animation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +26,13 @@ public class MainActivity extends AppCompatActivity {
         rlLanguageSelection = findViewById(R.id.rlLanguageSelection);
         btnNext = findViewById(R.id.btnShare);
 
+        moveProfle1Animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.move_profile1);
+        moveProfle2Animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.move_profile2);
+        moveProfle3Animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.move_profile3);
+
+        moveProfle1Animation.setAnimationListener(this);
+        moveProfle2Animation.setAnimationListener(this);
+        moveProfle3Animation.setAnimationListener(this);
 
         btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,20 +44,44 @@ public class MainActivity extends AppCompatActivity {
                         rlCircularWithUsers.setVisibility(View.VISIBLE);
                         break;
                     case 2:
-                        rlCircularWithUsers.findViewById(R.id.ivProfile1).setVisibility(View.GONE);
-                        rlCircularWithUsers.findViewById(R.id.btn1).setBackgroundColor(getResources().getColor(R.color.dark_red));
+                        rlCircularWithUsers.findViewById(R.id.ivProfile1).startAnimation(moveProfle1Animation);
                         break;
                     case 3:
-                        rlCircularWithUsers.findViewById(R.id.ivProfile2).setVisibility(View.GONE);
-                        rlCircularWithUsers.findViewById(R.id.btn2).setBackgroundColor(getResources().getColor(R.color.dark_orange));
+                        rlCircularWithUsers.findViewById(R.id.ivProfile2).startAnimation(moveProfle2Animation);
                         break;
                     case 4:
                         rlCircularWithUsers.findViewById(R.id.ivProfile3).setVisibility(View.GONE);
                         rlCircularWithUsers.findViewById(R.id.btn3).setBackgroundColor(getResources().getColor(R.color.dark_yellow));
+//                        rlCircularWithUsers.findViewById(R.id.ivProfile3).startAnimation(moveProfle3Animation); // could not animate as per required
                         break;
                     default:
                 }
             }
         });
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+        if (animation == moveProfle1Animation) {
+            rlCircularWithUsers.findViewById(R.id.ivProfile1).setVisibility(View.GONE);
+            rlCircularWithUsers.findViewById(R.id.btn1).setBackgroundColor(getResources().getColor(R.color.dark_red));
+
+        } else if (animation == moveProfle2Animation) {
+            rlCircularWithUsers.findViewById(R.id.ivProfile2).setVisibility(View.GONE);
+            rlCircularWithUsers.findViewById(R.id.btn2).setBackgroundColor(getResources().getColor(R.color.dark_orange));
+
+        } else if (animation == moveProfle3Animation) {
+//            rlCircularWithUsers.findViewById(R.id.btn3).setBackgroundColor(getResources().getColor(R.color.dark_yellow));
+//            rlCircularWithUsers.findViewById(R.id.ivProfile3).setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
     }
 }
